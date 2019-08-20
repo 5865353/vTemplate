759f87328f9e1ecd9ee54e03b20c19cfb4bd8b53
5865353 <865865353@qq.com>
2019-08-20 17:02:20 +0800

    update babelConfig

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,14 @@
 module.exports = {
     presets: [
-        ['@vue/app']
+        ['@vue/app', {
+        "polyfills":[
+            'es6.array.iterator',
+            'es6.promise',
+            'es6.object.assign',
+            "es6.set",
+            "es6.map",
+            'es7.promise.finally',
+        ]
+        }]
     ]
 }
